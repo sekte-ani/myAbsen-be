8400c581090fab9fc1dead328fb37a5700bad25e
MrGS <111638924+Gswaraw37@users.noreply.github.com>
2024-01-01 00:59:51 +0700

    Admin Kelar PLISS

--- resources/views/dashboard/modal/create.blade.php
+++ resources/views/dashboard/modal/create.blade.php
@@ -13,23 +13,23 @@
                     <div class="col-sm-12 col-md-12 col-lg-12">
                         <div class="mb-3">
                             <label for="name" class="form-label">Nama</label>
-                            <input type="text" class="form-control" name='name' id="name" placeholder="Masukkan Nama">
+                            <input type="text" class="form-control" name='name' id="name" placeholder="Masukkan Nama" required>
                         </div>
                         <div class="mb-3">
                           <label for="nomor_induk" class="form-label">Nomor Induk</label>
-                          <input type="text" name="nomor_induk" id="nomor_induk" placeholder="Masukkan Nomor Induk" class="form-control">
+                          <input type="text" name="nomor_induk" id="nomor_induk" placeholder="Masukkan Nomor Induk" class="form-control" required>
                         </div>
                         <div class="mb-3">
                             <label for="email" class="form-label">Email</label>
-                            <input type="email" name="email" id="email" placeholder="Masukkan Email" class="form-control">
+                            <input type="email" name="email" id="email" placeholder="Masukkan Email" class="form-control" required>
                         </div>
                         <div class="mb-3">
                           <label for="phone" class="form-label">No. HP</label>
-                          <input type="text" name="phone" id="phone" placeholder="Masukkan No. HP" class="form-control">
+                          <input type="text" name="phone" id="phone" placeholder="Masukkan No. HP" class="form-control" required>
                         </div>
                         <div class="mb-3">
                           <label for="address" class="form-label">Alamat</label>
-                          <input type="text" name="address" id="address" placeholder="Masukkan Alamat" class="form-control">
+                          <input type="text" name="address" id="address" placeholder="Masukkan Alamat" class="form-control" required>
                         </div>
                     </div>
                     
